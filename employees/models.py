3c4163e753001c82fa74707548ae125418913c65
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-24 00:07:59 -0300

    feat: creating and getting employees

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,4 @@
+import email
 from uuid import uuid4
 
 from django.core.validators import RegexValidator
@@ -6,13 +7,14 @@ from django.db import models
 
 class Employee(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=150)
     phone_number = models.CharField(
         max_length=20,
         validators=[
             RegexValidator(
-                regex=r"^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$",
-                message="The phone number must  follow the format: (xx) xxxxx-xxxx",
+                regex=r"^\([1-9]{2}\) 9[1-9][0-9]{3}-[0-9]{4}$",
+                message="The phone number must follow the format: (xx) 9xxxx-xxxx",
                 code=400,
             )
         ],
