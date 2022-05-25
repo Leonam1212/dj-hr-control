b18d84a0320f0bda24d1d6db86b26f71f5b57a16
Jerlysson <jerlyssonm@gmail.com>
2022-05-25 00:12:10 -0300

    feat accounts/serializer: function to put and patch feat accounts/urls: incremented routes to updated and delete account feat accounts/view: incremented view to update and delete fit hr_control/setting: descommented AUTH_USER

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
-
+from django.contrib.auth import authenticate, hashers
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from .models import Account
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -7,8 +9,31 @@ class AccountSerializer(serializers.ModelSerializer):
         model = Account
         fields = ['id','email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
-        
+
+    def create(self, validated_data):
+
+        validated_data["password"] = hashers.make_password(validated_data.get("password"))
+        return Account.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = hashers.make_password(validated_data.get('password', instance.password))
+        instance.save()
+        return instance
+
+
 class LoginSerializer(serializers.Serializer):
 
     email = serializers.CharField()
     password = serializers.CharField()
+
+    def authenticate(self):
+        self.is_valid(True)
+
+        user = authenticate(**self.data)
+        if not user:
+            raise AuthenticationFailed()
+        
+        token,_ = Token.objects.get_or_create(user=user)
+
+        return token.key
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
-from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework import generics
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from .permissions import IsRH
 from .models import Account
@@ -13,36 +12,19 @@ from .serializers import LoginSerializer,AccountSerializer
 class AccountView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsRH]
+
     queryset = Account.objects
     serializer_class = AccountSerializer
-    
-    def create(self, request: Request):
-        serializer = AccountSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
 
-        user = Account.objects.create(**serializer.validated_data)
-        user.set_password(serializer.validated_data["password"])           
-        user.save()
-
-        serializer = AccountSerializer(user)
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+class AccountUpdateAndDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects
+    serializer_class = AccountSerializer
 
 class LoginView(APIView):
-
+    serializer_class = LoginSerializer
     def post(self, request: Request):
-
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        
-        user = authenticate(
-            username = serializer.validated_data['email'],
-            password = serializer.validated_data['password']
-        )
-
-        if not user:
-            return Response({"message": "Invalid Credentials"}, status.HTTP_401_UNAUTHORIZED)
-        
-        token,_ = Token.objects.get_or_create(user=user)
-
-        return Response({"token": token.key})
+        try:
+            token = LoginSerializer(data=request.data).authenticate()
+            return Response({"token": token})
+        except (ValidationError, AuthenticationFailed) as err:
+            return Response(err.detail, err.status_code)
