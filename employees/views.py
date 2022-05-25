c4603be108167d04878c7d128b8cb71d6e941ede
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-25 14:09:59 -0300

    feat: requesting permission on employees views

--- employees/views.py
+++ employees/views.py
@@ -1,14 +1,18 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from accounts.permissions import IsRH
 from .models import Employee
 from .serializers import EmployeeSerializer
 
 
 class EmployeeView(generics.ListCreateAPIView):
+    permission_classes = [IsRH]
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
 
 class UpdateDestroyEmployeeView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsRH]
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = "id"
