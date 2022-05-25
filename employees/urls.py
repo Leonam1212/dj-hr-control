d05c57105f72003dfeb23c542efae666b1723637
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-25 11:23:32 -0300

    refactor: update and delete employee route path

--- employees/urls.py
+++ employees/urls.py
@@ -3,5 +3,5 @@ from .views import EmployeeView, UpdateDestroyEmployeeView
 
 urlpatterns = [
     path('employees/', EmployeeView.as_view()),
-    path('employees/<str:id>', UpdateDestroyEmployeeView.as_view()),
+    path('employees/<str:id>/', UpdateDestroyEmployeeView.as_view()),
 ]
