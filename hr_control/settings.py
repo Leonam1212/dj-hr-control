b7fa29d37bd6731b0e4c689d83724d77837d34aa
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-25 14:09:21 -0300

    feat: setting default auth class

--- hr_control/settings.py
+++ hr_control/settings.py
@@ -45,6 +45,12 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = ["rest_framework", "rest_framework.authtoken"]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    )
+}
+
 # Apps instalados
 
 MY_APPS = ["accounts", "employees", "contracts", "personal_documents", "candidates"]
