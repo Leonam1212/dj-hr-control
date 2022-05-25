46606ef1604b21701292885a6e8459fdcb3d966f
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-25 11:21:27 -0300

    refactor: emplyee view name

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EmployeeView, UpdateEmployeeView
-urlpatterns = [ #(4)
+from .views import EmployeeView, UpdateDestroyEmployeeView
+
+urlpatterns = [
     path('employees/', EmployeeView.as_view()),
-    path('employees/<str:id>', UpdateEmployeeView.as_view()),
+    path('employees/<str:id>', UpdateDestroyEmployeeView.as_view()),
 ]
