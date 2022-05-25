52ec88a7e59730892eeed412c6e7ca03f0e4b3c7
Leonam Rodrigues <leonam_5000@hotmail.com>
2022-05-25 16:40:28 -0300

    Fix(contract): Salary is an DecimalField

--- contracts/views.py
+++ contracts/views.py
@@ -1,13 +1,21 @@
 from rest_framework import generics
 from contracts.models import Contract
 from contracts.serializers import ContractSerializer
+from rest_framework.authentication import TokenAuthentication
+from accounts.permissions import IsRH
 
 class ContractView(generics.ListCreateAPIView):
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsRH]
+    
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
 
 
 class UpdateAndDeleteContractView(generics.RetrieveUpdateDestroyAPIView):
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsRH]
+
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
     lookup_field = "id"
--- hr_control/settings.py
+++ hr_control/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = "hr_control.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # SQLITE DB
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# POSTGRESQL DB
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": getenv("DB_NAME"),
-        "USER": getenv("DB_USER"),
-        "PASSWORD": getenv("DB_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# POSTGRESQL DB
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": getenv("DB_NAME"),
+#         "USER": getenv("DB_USER"),
+#         "PASSWORD": getenv("DB_PASSWORD"),
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
