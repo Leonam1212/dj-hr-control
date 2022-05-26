41ce8e9a7646f337bfd2f0c81d714057e5cb5b52
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-05-26 09:44:06 -0300

    feat(personal_document): create view update personal document

--- personal_documents/views.py
+++ personal_documents/views.py
@@ -6,3 +6,8 @@ from personal_documents.serializers import PersonalDocumentSerializer
 class PersonalDocumentsView(generics.ListCreateAPIView):
   queryset = Personal_document.objects.all()
   serializer_class = PersonalDocumentSerializer
+
+class PersonalDocumentByIdView(generics.UpdateAPIView):
+  queryset = Personal_document.objects.all()
+  serializer_class = PersonalDocumentSerializer
+  lookup_field = "id"
