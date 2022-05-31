df805c0c9ee61d10dafaa9543bf721cfd9af4632
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-05-31 14:09:39 -0300

    refactor: added unique constraint to name fild in shifts

--- shifts/models.py
+++ shifts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Shift(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     base_checkin = models.TimeField()
     base_checkout = models.TimeField()
