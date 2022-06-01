4eb496305a074a1909bf5a518d939bfa5e44f2f4
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-06-01 17:41:33 -0300

    feat: creating and sending PDF successfully

--- employees/views.py
+++ employees/views.py
@@ -1,14 +1,18 @@
-from tkinter import E
+from rest_framework.request import Request
+from rest_framework.response import Response
+from datetime import datetime
+from createPdf.jinja import createPDF
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import FileResponse
 from rest_framework import generics
+import calendar
+
 from accounts.permissions import IsRH
 from shifts.models import Shift
 from shifts.serializers import ShiftSerializer
 from .models import Employee
 from .serializers import EmployeeDetailedSerializer, EmployeeSerializer, EmployeeScheduleSerializer
-from rest_framework.request import Request
-from rest_framework.response import Response
-from datetime import datetime
-import calendar
+
 
 
 class EmployeeView(generics.ListCreateAPIView):
@@ -28,42 +32,55 @@ class UpdateDestroyEmployeeView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = "id"
 
 class CreateWorkScheduleView(generics.GenericAPIView):
-    def post(self, request: Request, type=""):
-       
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeScheduleSerializer
 
+    def get(self, request: Request, type=""):
+        try:
             employee_list = Employee.objects.all()
             employees = EmployeeScheduleSerializer(employee_list, many=True)
             shift_list = Shift.objects.all()
             shifts = ShiftSerializer(shift_list, many=True)
             newSchedule = createSchedule(employees.data, shifts.data)
+            if not employee_list:
+                raise ObjectDoesNotExist
 
-            return Response(newSchedule)
+            if type == "json":
+                return Response(newSchedule)
+            if type == "pdf":
+                createPDF(newSchedule)
+                return FileResponse(open('createPdf/output/file.pdf', 'rb'))
+            return Response({"error": "type not found."})
+        except(ObjectDoesNotExist):
+            return Response({"detail": "Employee not found"}, status.HTTP_404_NOT_FOUND)
+
+            
         
 def createSchedule(employees ,shifts):
     date = datetime.now()
     month = date.month
     month_range = calendar.monthrange(date.year, date.month)
     month_name = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
+        1: "Janeiro",
+        2: "Fevereiro",
+        3: "Março",
+        4: "Abril",
+        5: "Maio",
+        6: "Juinho",
+        7: "Julho",
+        8: "Agosto",
+        9: "Setembro",
+        10: "Outubro",
+        11: "Novembro",
+        12: "Dezembro"
     }
     week_name = {
-        6: "Sunday",
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
+        6: "Segunda-feira",
+        0: "Terça-feira",
+        1: "Quarta-feira",
+        2: "Quinta-feira",
+        3: "Sexta-feira",
+        4: "Sabado",
         5: "Saturday"
     }
 
@@ -71,11 +88,9 @@ def createSchedule(employees ,shifts):
     def schedule():
         table = {}
         
-
-
         for day in range(month_range[1]):
             days = {}
-            days["week_day"] = week_name[calendar.weekday(date.year, date.month, day+1)]
+            days["week_day"] = [week_name[calendar.weekday(date.year, date.month, day+1)]]
             
             for shift in shifts:
                 days[shift["name"]] = []
